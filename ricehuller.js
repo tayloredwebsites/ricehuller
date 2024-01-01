bebac65c7c21446d57da355e44ae463d213e561c
Dave Taylor <tayloredwebsites@me.com>
2024-01-01 14:49:38 -0500

    - Logo with green bowl, green header backgrounds - starting piranha huller page with html device responsive table - sections under articles - unobtrusive js expand and collapse of articles with expand-collapse class

--- ricehuller.js
+++ ricehuller.js
@@ -1,19 +1,35 @@
 /* *************************************************************************************
+window onload event handler function
+*/
+function riceHullerOnLoad() {
+  // set the onclick event on the elements with a class of expand-collapse
+  elementsByQuery(".expand-collapse").addEventListener("click", (event) => expanderCollapser(event));
+}
+
+/* *************************************************************************************
 (global) constants
 */
 
-const MY_CONSTANT = "some-value";
+// const MY_CONSTANT = "some-value";
 
 /* *************************************************************************************
 helper methods
 */
 
-function getById(tagId) {
+function elementById(tagId) {
   return document.getElementById(tagId);
 }
 
-function getByName(tagName) {
+function elementsByName(tagName) {
   return document.getElementsByTagName(tagName);
+}
+
+function elementsByClass(tagClass) {
+  return document.getElementsByClassName(tagClass);
+}
+
+function elementsByQuery(selector) {
+  return document.querySelector(selector)
 }
 
 function setErrorMsg(errMsg) {
@@ -29,10 +45,39 @@ function setErrorMsg(errMsg) {
 
 
 /* *************************************************************************************
-onclick detections
+unobtrusive onclick detections, which must be called in the window.onload event (must be called only once)
+see riceHullerOnLoad function called in html script tag, and at the top of this document
 */
 
-// getById("smaller-font").onclick = function() {setFontSize(-2)};
+function expanderCollapser(event) {
+  console.log("Clicked an element with the expand-collapse class")
+  const elem = event.currentTarget;
+  console.log("called expanderCollapser on element with with tag: ".concat(elem.tagName));
+  const wasExpanded = elem.classList.contains("expanded");
+  console.log("was element initially expanded: ".concat(wasExpanded.toString()));
+
+  // var headerElem = null;
+  // var sectionElem = null;
+  // // get the SECTION and HEADER children of the element clicked on
+  // for (let child of elem.children) {
+  //   console.log("checking sibing with tag: ".concat(child.tagName));
+  //   if (child.tagName === "SECTION") {
+  //     console.log("We MATCHED a sibing with SECTION tag");
+  //     sectionElem = child;
+  //   }  else if (child.tagName === "HEADER") {
+  //     console.log("We MATCHED a sibing with HEADER tag");
+  //     headerElem = child;
+  //   };
+  //   if (sectionElem !== null && headerElem !== null) { break };
+  // };
+  console.log("Initial element classList: ".concat(elem.classList));
+  if (wasExpanded === true) {
+    elem.classList.remove("expanded");
+  } else {
+    elem.classList.add("expanded");
+  }
+
+};
 
 
 /* *************************************************************************************
